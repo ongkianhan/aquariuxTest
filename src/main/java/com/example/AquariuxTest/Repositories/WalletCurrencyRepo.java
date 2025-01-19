d6c1b8e1d35b2faa97bcc83e36cc0125b00a6962
Kent Ong Kian Han <75796933+ongkianhan@users.noreply.github.com>
2025-01-20 05:26:58 +0800

    Fixed some warnings

--- src/main/java/com/example/AquariuxTest/Repositories/WalletCurrencyRepo.java
+++ src/main/java/com/example/AquariuxTest/Repositories/WalletCurrencyRepo.java
@@ -5,8 +5,6 @@ import com.example.AquariuxTest.Entity.WalletCurrPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface WalletCurrencyRepo extends JpaRepository<WalletCurr, WalletCurrPK> {
     WalletCurr findByIdAndTicker(Long id, String ticker);
