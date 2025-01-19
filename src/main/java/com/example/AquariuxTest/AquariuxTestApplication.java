635db51aac655b5696e47c6a6914ad9981a7551a
Kent Ong Kian Han <75796933+ongkianhan@users.noreply.github.com>
2025-01-19 17:14:36 +0800

    First commit for spring project

---
+++ src/main/java/com/example/AquariuxTest/AquariuxTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.AquariuxTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquariuxTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquariuxTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AquariuxTest/AquariuxTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AquariuxTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquariuxTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
